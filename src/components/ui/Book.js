b15203fd7d3163033c899a29249d857864060af7
Lisa <lisa.eriksson.w@gmail.com>
2019-04-12 10:52:55 +0200

    Changed book, not finished

--- src/components/ui/Book.js
+++ src/components/ui/Book.js
@@ -16,14 +16,27 @@ class Book extends Component {
 
         return (
             <li className="list-item list-group-item d-flex align-items-center">
-                <strong className="title">{this.state.title}</strong>
-            <div className="author">{this.state.author}</div>
+                {this.props.editMode ? <input onChange={this.props.titleHandler}/> : 
+                <strong classname="title">{this.state.title}</strong>}
+            
+                {this.props.editMode ? <input onChange={this.props.authorHandler}/> : 
+                <div className="author">{this.state.author}</div>}
+            
             <div className="buttons">
+
+            {this.props.editMode ? 
+                <button type="button"
+                    className="btn btn-success"
+                    onClick={this.props.submitEdit}>
+                    Spara
+                </button>
+                :
                 <button type="button"
                     className="btn btn-success"
                     onClick={this.props.toggleEdit}>
                     Editera
                 </button>
+                }
                 <button type="button"
                     className="btn btn-danger"
                     onClick={() => this.props.deleteBook(this.state.id)}>
